b52a3346fcf2e35d5dfd117c671acfdafc79eadf
vatsalpatel2503 <vatsalpansuriya25@gmail.com>
2023-06-26 08:38:57 +0530

    This is my third file commit

---
+++ file3.py
@@ -0,0 +1,15 @@
+# if-else statement
+if x > 0:
+    print("Positive")
+else:
+    print("Non-positive")
+
+# for loop
+for num in numbers:
+    print(num)
+
+# while loop
+count = 0
+while count < 5:
+    print(count)
+    count += 1
