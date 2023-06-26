593dc9bb895c2addcd59bd99fa74b655dce3be8e
vatsalpatel2503 <vatsalpansuriya25@gmail.com>
2023-06-26 08:39:33 +0530

    This is my forth file commit

---
+++ file4.py
@@ -0,0 +1,15 @@
+# Numeric data types
+age = 25
+price = 9.99
+
+# String data type
+name = "Alice"
+
+# Boolean data type
+is_valid = True
+
+# List data type
+numbers = [1, 2, 3, 4, 5]
+
+# Dictionary data type
+person = {"name": "John", "age": 30}
